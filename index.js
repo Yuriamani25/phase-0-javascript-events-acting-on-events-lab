f031b438924529ec621e8919f09094129fb8756e
YuriAmani <yuriamani7@gmail.com>
2024-04-08 15:23:52 +0300

    js-events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+//add event listener
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+  
+  //move dodger left
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  //console.log(left);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  //move dodger right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  //console.log(left);
+    if (left < 359) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
